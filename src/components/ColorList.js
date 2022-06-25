da364a5c0eafcc9ffbd2b1eadc07d1d66431e0e7
Samuel Banya <sbanya@fastmail.com>
2022-06-25 11:21:54 -0400

    Adding latest changes to assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+// Assignment example using normal 'props':
+function ColorItem1(props) {
+  return <li style={{ color: props.color}}>{props.color}</li>;
+}
+
+// My revised example using destructuring:
+function ColorItem2({ color }) {
+  return <li style={{ color: color }}>{color}</li>;
+}
+
 function ColorList() {
   const colors = [
     "firebrick",
@@ -9,15 +19,36 @@ function ColorList() {
     "hotpink",
   ];
 
+  // NOTE: Since we are using '.map()' to create an array of JSX elements, we HAVE to use
+  // the 'key' prop for each element of the array
+
+  // ALSO: Ideally, the 'key' would unique 'id' values in a normal scenario
+
+  // Original version:
+  const colorElements1 = colors.map((color) => {
+    return <ColorItem1 key={color} color={color} />;
+  });
+
+  // My version of 'colorElements' using the destructured version of 'props'
+  const colorElements2 = colors.map((color) => {
+    return <ColorItem2 key={color} color={color} />;
+
+    // NOTE: This is the 'return' statement if we did NOT use a separate component above:
+    /*
+    return (
+        <li key={color} style={{ color: color}}>
+        {color}
+      </li>
+      );
+      */
+  });
+
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+      {colorElements1}
+      {colorElements2}
       </ol>
     </div>
   );
